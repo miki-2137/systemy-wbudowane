1a00bd4bf1dee8dff8ef82a0402d6148bcbacff3
Mikołaj Małecki <92546137+miki-2137@users.noreply.github.com>
2026-05-20 10:53:13 +0200

    Update zad4.c

--- zad4.c
+++ zad4.c
@@ -1,33 +1,43 @@
-#pragma config POSCMOD = XT
-#pragma config OSCIOFNC = ON
-#pragma config FCKSM = CSDCMD
-#pragma config FNOSC = PRI
-#pragma config IESO = ON
- 
-#pragma config WDTPS = PS32768
-#pragma config FWPSA = PR128
-#pragma config WINDIS = ON
-#pragma config FWDTEN = ON
-#pragma config ICS = PGx2
-#pragma config GWRP = OFF
-#pragma config GCP = OFF
-#pragma config JTAGEN = OFF
- 
-// Zmienne globalne
-unsigned int powerIndex = 0;
-unsigned int powerLevels[4] = {800, 600, 350, 200};
+// PIC24FJ128GA010 Configuration Bit Settings
 
-unsigned long czas = 0; 
-unsigned char dziala = 0; // 0 - Stop, 1 - Gotowanie
+// 'C' source line config statements
+
+// CONFIG2
+#pragma config POSCMOD = XT             // Primary Oscillator Select (XT Oscillator mode selected)
+#pragma config OSCIOFNC = ON            // Primary Oscillator Output Function (OSC2/CLKO/RC15 functions as port I/O (RC15))
+#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Clock switching and Fail-Safe Clock Monitor are disabled)
+#pragma config FNOSC = PRI              // Oscillator Select (Primary Oscillator (XT, HS, EC))
+#pragma config IESO = ON                // Internal External Switch Over Mode (IESO mode (Two-Speed Start-up) enabled)
+
+// CONFIG1
+#pragma config WDTPS = PS32768          // Watchdog Timer Postscaler (1:32,768)
+#pragma config FWPSA = PR128            // WDT Prescaler (Prescaler ratio of 1:128)
+#pragma config WINDIS = ON              // Watchdog Timer Window (Standard Watchdog Timer enabled,(Windowed-mode is disabled))
+#pragma config FWDTEN = ON              // Watchdog Timer Enable (Watchdog Timer is enabled)
+#pragma config ICS = PGx2               // Comm Channel Select (Emulator/debugger uses EMUC2/EMUD2)
+#pragma config GWRP = OFF               // General Code Segment Write Protect (Writes to program memory are allowed)
+#pragma config GCP = OFF                // General Code Segment Code Protect (Code protection is disabled)
+#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG port is disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+#include <xc.h>
+#include "libpic30.h"
+#include "lcd.h"
+#include "adc.h"
+
+unsigned int moc[4] = {800, 600, 350, 200};
+unsigned int wybrana = 0;
+unsigned long czas = 0;
 
 void init(void)
 {
-    AD1PCFG = 0xFFFF; // Wszystkie piny analogowe jako cyfrowe
+    AD1PCFG = 0xFFFF;
     
-    TRISA = 0x0000;   // Port A na wyjscie - diody
+    TRISA = 0x0000;
     LATA = 0x0000;
     
-    // Konfiguracja przycisków RB2, RB3, RB4, RB5 na wejście
     TRISD |= (1 << 6) | (1 << 7) | (1 << 13);
     TRISAbits.TRISA7 = 1;
 
@@ -36,41 +46,41 @@ void init(void)
 
 void wyswietl(void)
 {
-    unsigned int mm = czas / 60;
-    unsigned int ss = czas % 60;
+    unsigned int min = czas / 60;
+    unsigned int sek = czas % 60;
 
     LCD_ClearScreen();
     LCD_PutString("Moc: ", 5);
 
-    if(powerLevels[powerIndex] == 800) LCD_PutString("800W", 4);
-    else if(powerLevels[powerIndex] == 600) LCD_PutString("600W", 4);
-    else if(powerLevels[powerIndex] == 350) LCD_PutString("350W", 4);
+    if(moc[wybrana] == 800) LCD_PutString("800W", 4);
+    else if(moc[wybrana] == 600) LCD_PutString("600W", 4);
+    else if(moc[wybrana] == 350) LCD_PutString("350W", 4);
     else LCD_PutString("200W", 4);
 
     LCD_PutChar('\n');
 
     LCD_PutString("Czas: ", 6);
-    LCD_PutChar('0' + (mm / 10));
-    LCD_PutChar('0' + (mm % 10));
+    LCD_PutChar('0' + (min / 10));
+    LCD_PutChar('0' + (min % 10));
     LCD_PutChar(':');
-    LCD_PutChar('0' + (ss / 10));
-    LCD_PutChar('0' + (ss % 10));
+    LCD_PutChar('0' + (sek / 10));
+    LCD_PutChar('0' + (sek % 10));
 }
 
 void wyswietlGotowanie(void)
 {
-    unsigned int mm = czas / 60;
-    unsigned int ss = czas % 60;
+    unsigned int min = czas / 60;
+    unsigned int sek = czas % 60;
 
     LCD_ClearScreen();
-    LCD_PutString("Gotowanie...", 12);
+    LCD_PutString("Gotowanie", 9);
     LCD_PutChar('\n');
 
-    LCD_PutChar('0' + (mm / 10));
-    LCD_PutChar('0' + (mm % 10));
+    LCD_PutChar('0' + (min / 10));
+    LCD_PutChar('0' + (min % 10));
     LCD_PutChar(':');
-    LCD_PutChar('0' + (ss / 10));
-    LCD_PutChar('0' + (ss % 10));
+    LCD_PutChar('0' + (sek / 10));
+    LCD_PutChar('0' + (sek % 10));
 }
 
 int main(void)
@@ -78,57 +88,53 @@ int main(void)
     init();
     wyswietl();
 
-    // Zmienne do stanów przycisków
     unsigned char poprzedni_rd6 = 1, stan_rd6 = 1;
     unsigned char poprzedni_rd7 = 1, stan_rd7 = 1;
     unsigned char poprzedni_ra7 = 1, stan_ra7 = 1;
     unsigned char poprzedni_rd13 = 1, stan_rd13 = 1;
+    
+    unsigned char wlaczona = 0;
 
     while(1)
     {
-        // Tryb ustawiania kuchenki
-        if(dziala == 0)
+        if(wlaczona == 0)
         {
             poprzedni_rd6 = PORTDbits.RD6;
             poprzedni_rd7 = PORTDbits.RD7;
             poprzedni_ra7 = PORTAbits.RA7;
             poprzedni_rd13 = PORTDbits.RD13;
 
-            __delay32(50000); // prosty debouncing z wzoru
+            __delay32(50000);
 
             stan_rd6 = PORTDbits.RD6;
             stan_rd7 = PORTDbits.RD7;
             stan_ra7 = PORTAbits.RA7;
             stan_rd13 = PORTDbits.RD13;
 
-            // Zmiana mocy (RB5)
             if(poprzedni_rd6 == 1 && stan_rd6 == 0)
             {
-                powerIndex++;
-                if(powerIndex > 3) powerIndex = 0;
+                wybrana++;
+                if(wybrana > 3) wybrana = 0;
                 wyswietl();
             }
 
-            // Dodaj 1 minutę (RB4)
             if(poprzedni_rd7 == 1 && stan_rd7 == 0)
             {
                 czas += 60;
                 wyswietl();
             }
 
-            // Dodaj 10 sekund (RB3)
             if(poprzedni_ra7 == 1 && stan_ra7 == 0)
             {
                 czas += 10;
                 wyswietl();
             }
 
-            // Start (RB2)
             if(poprzedni_rd13 == 1 && stan_rd13 == 0)
             {
                 if(czas > 0)
                 {
-                    dziala = 1;
+                    wlaczona = 1;
                     wyswietlGotowanie();
                 }
             }
@@ -137,46 +143,35 @@ int main(void)
         {
             poprzedni_rd13 = PORTDbits.RD13;
             
-            __delay32(1000000);
+            __delay32(4000000);
             
             stan_rd13 = PORTDbits.RD13;
 
-            // Przerwanie gotowania (RB2)
             if(poprzedni_rd13 == 1 && stan_rd13 == 0)
             {
-                dziala = 0;
+                wlaczona = 0;
                 wyswietl();
                 continue; 
             }
 
-            // Odliczanie czasu
             if(czas > 0)
             {
                 czas--;
                 wyswietlGotowanie();
             }
 
-            // Zakończenie pracy
             if(czas == 0)
             {
-                dziala = 0;
+                wlaczona = 0;
 
                 LCD_ClearScreen();
-                LCD_PutString("Koniec!", 7);
+                LCD_PutString("Koniec!!!", 9);
                 LCD_PutChar('\n');
                 LCD_PutString("00:00", 5);
 
-                // Mruganie diodami na koniec pracy
-                
-                for(int i = 0; i < 6; i++)
-                {
-                    LATA = 0x00FF; // Zapala 8 diod dla lepszego efektu
-                    __delay32(300000);
-                    LATA = 0x0000;
-                    __delay32(300000);
-                }
+                __delay32(10000000);
 
-                wyswietl(); // Powrót do menu startowego
+                wyswietl();
             }
         }
     }
